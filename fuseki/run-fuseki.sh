9103b8fda93eff638b0d83177cd7a25e11c6d7d5
Mike Ledger <eleventynine@gmail.com>
2016-04-22 06:28:55 +1000

    be nice to your cd

--- fuseki/run-fuseki.sh
+++ fuseki/run-fuseki.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 set -e
-
+cd "$(dirname $0)"
 . get-fuseki.sh
-export FUSEKI_BASE=`pwd`/fuseki-base
+export FUSEKI_BASE="$(pwd)/fuseki-base"
 export FUSEKI_HOME="$DEST_DIR"
 mkdir -p "$FUSEKI_BASE"
 exec "$DEST_DIR/fuseki-server" $@
+
