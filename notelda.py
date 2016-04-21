269f0578921ac83fafd6dab6a0e21daed0fffb61
Mike Ledger <eleventynine@gmail.com>
2016-04-22 06:28:07 +1000

    don't do a reverse proxy's job worse than a reverse proxy

--- notelda.py
+++ notelda.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 from screw_rdflib import ConjunctiveGraph, Literal, Namespace, OWL, RDF, RDFS, XSD, URIRef, BNode, Graph
-from app import app, manager
+from app          import app, manager
+from flask        import request
 
 query_url = 'http://localhost:3030/landsat/query'
 update_url = 'http://localhost:3030/landsat/update'
 fuseki = ConjunctiveGraph(store='SPARQLUpdateStore')
 fuseki.open((query_url, update_url))
-
-@app.route('/query/<string:query>')
-def query(string):
-    return fuseki.query(string)
 
 @app.route('/query/landsat/<int:uid>')
 def uid(uid):
