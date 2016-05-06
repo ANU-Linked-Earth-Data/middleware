5c3205fef0bfb344bb06fa09f4626e475a45ff00
Sam Toyer <sam@qxcv.net>
2016-05-06 14:20:12 +1000

    Test actually runs now

--- dynamic-store/src/main/java/anuled/dynamicstore/TileReader.java
+++ dynamic-store/src/main/java/anuled/dynamicstore/TileReader.java
@@ -3,8 +3,6 @@ package anuled.dynamicstore;
 // gdal gdal? gdal. gdal gdal gdal gdal!
 import org.gdal.gdal.gdal;
 
-import anuled.dynamicstore.TileReader.Pixel;
-
 import java.util.Iterator;
 
 import org.gdal.gdal.Band;
@@ -15,75 +13,65 @@ import org.gdal.gdal.Dataset;
  * manipulating it.
  */
 public class TileReader {
-	
-	public static void main(String[] args) {
-		TileReader tr = new TileReader(args[0]);
-		for (Pixel pixel : tr.pixels()) {
-			System.out.println(pixel.toString());
-		}
-	}
-	
-	public Dataset  dataset;
-	public int      bands;
-	public int      pixelWidth;
-	public int      pixelHeight;
+	public Dataset dataset;
+	public int bands;
+	public int pixelWidth;
+	public int pixelHeight;
 	/** Affine transform coefficients **/
 	public double[] transform;
-	
+
 	public TileReader(String filename) {
-		dataset   = gdal.Open(filename);
+		dataset = gdal.Open(filename);
 		transform = dataset.GetGeoTransform();
 		bands = dataset.getRasterCount();
 		pixelWidth = dataset.getRasterXSize();
 		pixelHeight = dataset.getRasterYSize();
 	}
-	
+
 	public double[] pixelLatLon(int row, int col, double[] latlong) {
 		latlong[0] = transform[3] + col * transform[4] + row * transform[5];
 		latlong[1] = transform[0] + col * transform[1] + row * transform[2];
 		return latlong;
 	}
-	
-	public double[] pixelLatLon(int row, int col) { 
+
+	public double[] pixelLatLon(int row, int col) {
 		return pixelLatLon(row, col, new double[2]);
 	}
-	
-	
+
 	public short pixelValue(int band_no, int row, int col) {
 		Band band = dataset.GetRasterBand(band_no);
 		short[] retArray = {0};
 		band.ReadRaster(row, col, 1, 1, retArray);
 		return retArray[0];
 	}
-	
+
 	public class Pixel {
 		public int row, col;
-		public short[]  pixel;
+		public short[] pixel;
 		public double[] latlong;
-		
+
 		@Override
 		public String toString() {
-			return "Pixel(" + 
-					latlong[0] + "N , " + latlong[1] + "W, " + 
-					shortsAsHex(pixel) + ")";
+			return "Pixel(" + latlong[0] + "N , " + latlong[1] + "W, "
+					+ shortsAsHex(pixel) + ")";
 		}
-		
+
 		public Pixel(int row, int col) {
-			this.row     = row;
-			this.col     = col;
-			this.pixel   = TileReader.this.pixel(row, col);
+			this.row = row;
+			this.col = col;
+			this.pixel = TileReader.this.pixel(row, col);
 			this.latlong = TileReader.this.pixelLatLon(row, col);
 		}
 	}
-	
+
 	public short[] pixel(int row, int col) {
-		short[] pixel = new short[bands]; 
-		for(int b = 0; b < bands; b ++) {
-			pixel[b] = pixelValue(b, row, col);
+		short[] pixel = new short[bands];
+		for (int b = 0; b < bands; b++) {
+			pixel[b] = pixelValue(b+1, row, col);
 		}
 		return pixel;
 	}
-	
+
 	/**
 	 * Iterable over all pixels in this TileReader
 	 */
@@ -93,20 +81,20 @@ public class TileReader {
 			public Iterator<Pixel> iterator() {
 				return new Iterator<Pixel>() {
 					private int row = 0, col = 0;
-					
+
 					@Override
 					public Pixel next() {
 						Pixel pixel = new Pixel(row, col);
-						
-						if(row < pixelWidth)
-							row ++;
+
+						if (row < pixelWidth)
+							row++;
 						else
 							row = 0;
-							col ++;
-							
+						col++;
+
 						return pixel;
 					}
-					
+
 					@Override
 					public boolean hasNext() {
 						return col < pixelHeight;
@@ -115,14 +103,13 @@ public class TileReader {
 			}
 		};
 	}
-	
+
 	public static String shortsAsHex(short[] thing) {
 		StringBuilder builder = new StringBuilder("0x");
-		for(short s : thing) {
+		for (short s : thing) {
 			builder.append(Integer.toHexString(s & 0xffff));
 		}
 		return "0x" + builder.toString();
 	}
 
 }
-
--- dynamic-store/src/test/java/anuled/dynamicstore/TestTileReader.java
+++ dynamic-store/src/test/java/anuled/dynamicstore/TestTileReader.java
@@ -1,9 +1,14 @@
 package anuled.dynamicstore;
 
+import java.io.InputStream;
 import java.net.URL;
-import java.util.Enumeration;
 
+import org.gdal.gdal.gdal;
 import org.junit.Test;
+
+import anuled.dynamicstore.TileReader.Pixel;
+
+import org.apache.commons.io.IOUtils;
 
 import junit.framework.TestCase;
 
@@ -13,19 +18,44 @@ import junit.framework.TestCase;
  */
 public class TestTileReader extends TestCase {
 	private static String TEST_FILE_NAME = "/LS7_ETM_NBAR_149_-035_2013-02-21T23-46-15.883624.small.tif";
+	private static String TEST_MEM_PATH = "/vsimem" + TEST_FILE_NAME;
+	private static int TRUE_WIDTH = 32;
+	private static int TRUE_HEIGHT = 32;
 	private TileReader reader;
 
 	protected void setUp() throws Exception {
 		super.setUp();
+		
 		Class<? extends TestTileReader> cls = this.getClass();
 		URL res = cls.getResource(TEST_FILE_NAME);
-		String filePath = res.toURI().toString();
-		reader = new TileReader(filePath);
+		res.toURI().toURL();
+		InputStream stream = res.openStream();
+		byte[] testFileBytes = IOUtils.toByteArray(stream);
+
+		gdal.AllRegister();
+		gdal.FileFromMemBuffer(TEST_MEM_PATH, testFileBytes);
+		
+		reader = new TileReader(TEST_MEM_PATH);
+	}
+	
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		gdal.Unlink(TEST_MEM_PATH);
 	}
 	
 	@Test
 	public void testSize() {
-		assertEquals(reader.pixelHeight, 32);
-		assertEquals(reader.pixelWidth, 32);
+		assertEquals(reader.pixelHeight, TRUE_HEIGHT);
+		assertEquals(reader.pixelWidth, TRUE_WIDTH);
+	}
+	
+	@Test
+	public void testPixelIterator() {
+		Iterable<Pixel> iter = reader.pixels();
+		int numPixels = 0;
+		for (@SuppressWarnings("unused") Pixel pixel : iter) {
+			numPixels += 1;
+		}
+		assert(numPixels == TRUE_HEIGHT * TRUE_WIDTH);
 	}
 }
