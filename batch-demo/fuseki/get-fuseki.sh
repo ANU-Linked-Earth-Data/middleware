b337ace31be7565b0b2fd83a8ade1e24936e3ed0
Ubuntu <ubuntu@elda-prod.novalocal>
2016-05-09 05:28:46 +0000

    Mirrors go down :/

--- batch-demo/fuseki/get-fuseki.sh
+++ batch-demo/fuseki/get-fuseki.sh
@@ -6,7 +6,8 @@ VERSION=2.3.1
 DEST_TGZ="apache-jena-fuseki-${VERSION}.tar.gz"
 DEST_TGZ_MD5SUM="b8ee1ecfb47849550f9412450606c606  $DEST"
 DEST_DIR="$(pwd)/apache-jena-fuseki-${VERSION}"
-URL="http://apache.mirror.serversaustralia.com.au/jena/binaries/apache-jena-fuseki-${VERSION}.tar.gz"
+APACHE_REPO_PATH="/jena/binaries/apache-jena-fuseki-${VERSION}.tar.gz"
+MIRRORS="apache.mirror.serversaustralia.com.au apache.uberglobalmirror.com apache.mirror.digitalpacific.com.au"
 
 if [ ! -d "$DEST_DIR" ]; then
     echo "Calculating checksum for $DEST_TGZ"
@@ -14,7 +15,15 @@ if [ ! -d "$DEST_DIR" ]; then
         echo "$DEST_TGZ exists, and checksum is fine, skipping download"
     else
         echo "Re-downloading to $DEST_TGZ"
-        wget -O "$DEST_TGZ" "$URL"
+        for mirror in $MIRRORS; do
+            URL="http://$mirror/$APACHE_REPO_PATH"
+            echo "Attempting download from $URL"
+            wget -O "$DEST_TGZ" "$URL" && break || echo "Download failed"
+        done
+        if [ ! -f "$DEST_TGZ" ]; then
+            echo "Failed to download from any mirror"
+            exit 1
+        fi
     fi
 
     tar xf "$DEST_TGZ"
