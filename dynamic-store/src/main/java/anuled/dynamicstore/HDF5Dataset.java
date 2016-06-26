42ff391770642f0c083c35114c38d49a97f93571
Sam Toyer <sam@qxcv.net>
2016-06-27 00:25:03 +1000

    Use JHDF5 instead

--- dynamic-store/src/main/java/anuled/dynamicstore/HDF5Dataset.java
+++ dynamic-store/src/main/java/anuled/dynamicstore/HDF5Dataset.java
@@ -1,27 +1,69 @@
 package anuled.dynamicstore;
 
-import ncsa.hdf.hdf5lib.exceptions.HDF5Exception;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import ch.systemsx.cisd.hdf5.HDF5Factory;
+import ch.systemsx.cisd.hdf5.IHDF5Reader;
 
 /* Class for accessing satellite observations stored using our custom HDF5 format. */
 
-import ncsa.hdf.object.h5.H5File;
-
 public class HDF5Dataset {
-	private H5File fp;
+	private IHDF5Reader fp;
+	private Collection<HDF5Cell> cells;
 	
 	public HDF5Dataset(String filename) {
-		fp = new H5File(filename, H5File.READ);
+		fp = HDF5Factory.openForReading(filename);
+		// Read all cells into core (but not their data). This makes our job a bit easier later on.
+		cells = populateCells();
+	}
+	
+	private Collection<HDF5Cell> populateCells() {
+		Queue<String> to_explore = new LinkedList<String>();
+		to_explore.add("/");
+		assert fp.isGroup(to_explore.peek());
+		
+		// Populate list of HDF5Cells (albeit only ones with data in them!)
+		Collection<HDF5Cell> rv = new ArrayList<HDF5Cell>();
+		while (!to_explore.isEmpty()) {
+			String group = to_explore.remove();
+			
+			// Append this node to the index
+			HDF5Cell cell = new HDF5Cell(group);
+			rv.add(cell);
+			
+			// Now add children to explore
+			for (String child : fp.getGroupMembers(group)) {
+				if (fp.isGroup(child)) {
+					to_explore.add(child);
+				}
+			}
+		}
+		return rv;
 	}
 	
 	public void dispose() {
-		try {
-			fp.close();
-		} catch (HDF5Exception e) {
-			throw new RuntimeException("Could not close file (see inner)", e);
+		fp.close();
+	}
+	
+	public class HDF5Cell {
+		String path;
+		
+		private HDF5Cell(String path) {
+			this.path = path;
 		}
 	}
 	
 	public Iterable<HDF5Cell> cells() {
-		return null;
+		return new Iterable<HDF5Cell>() {
+			@Override
+			public Iterator<HDF5Cell> iterator() {
+				return cells.iterator();
+			}
+		};
 	}
 }
--- dynamic-store/src/test/java/anuled/dynamicstore/TestHDF5Dataset.java
+++ dynamic-store/src/test/java/anuled/dynamicstore/TestHDF5Dataset.java
@@ -1,22 +1,64 @@
 package anuled.dynamicstore;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
 import org.junit.After;
+import org.junit.AfterClass;
 
 public class TestHDF5Dataset {
 	HDF5Dataset ds;
+	static File tempFile;
+	
+	@BeforeClass
+	public static void setUpClass() throws IOException {
+		Class<? extends TestHDF5Dataset> cls = TestHDF5Dataset.class;
+		InputStream readStream = cls.getResourceAsStream(TestData.TEST_H5_NAME);
+		try {
+			String baseName = new File(TestData.TEST_H5_NAME).getName();
+			tempFile = File.createTempFile(baseName, "");
+			tempFile.deleteOnExit(); // defensive; we delete on tearDown anyway
+
+			FileOutputStream outStream = new FileOutputStream(tempFile);
+			try {
+				byte[] buffer = new byte[8192];
+				while (true) {
+					int numRead = readStream.read(buffer);
+					if (numRead <= 0) {
+						break;
+					}
+					byte[] writeBuffer = Arrays.copyOfRange(buffer, 0, numRead);
+					outStream.write(writeBuffer);
+				}
+			} finally {
+				outStream.close();
+			}
+		} finally {
+			readStream.close();
+		}
+	}
 	
 	@Before
 	public void setUp() {
-		ds = new HDF5Dataset(TestData.TEST_H5_NAME);
+		ds = new HDF5Dataset(tempFile.getAbsolutePath());
 	}
 	
 	@After
 	public void tearDown() {
 		ds.dispose();
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+		tempFile.delete();
 	}
 	
 	@Test
@@ -26,8 +68,11 @@ public class TestHDF5Dataset {
 	
 	@Test
 	public void testCellIterator() {
-		for (HDF5Cell cell : ds.cells()) {
-			
+		int numCells = 0;
+		for (HDF5Dataset.HDF5Cell cell : ds.cells()) {
+			assertNotNull(cell);
+			numCells = numCells + 1;
 		}
+		// assertEquals(2, numCells);
 	}
 }
