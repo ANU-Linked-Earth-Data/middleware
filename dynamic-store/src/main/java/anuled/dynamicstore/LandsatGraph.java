994fb1cbecd0fa00b53ba7b176f9c3ff68d7b421
Sam Toyer <sam@qxcv.net>
2016-06-29 23:22:37 +1000

    Move metadata to Turtle file

--- dynamic-store/src/main/java/anuled/dynamicstore/LandsatGraph.java
+++ dynamic-store/src/main/java/anuled/dynamicstore/LandsatGraph.java
@@ -1,23 +1,19 @@
 package anuled.dynamicstore;
 
-import java.util.function.Function;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.stream.Stream;
+
 import org.apache.jena.graph.FrontsTriple;
 import org.apache.jena.graph.Triple;
 import org.apache.jena.graph.impl.GraphBase;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.util.iterator.ExtendedIterator;
 import org.apache.jena.vocabulary.RDF;
-import org.apache.jena.vocabulary.RDFS;
-import org.apache.jena.vocabulary.XSD;
 
 import anuled.dynamicstore.HDF5Dataset.Observation;
-import anuled.vocabulary.GCMDInstrument;
-import anuled.vocabulary.GCMDPlatform;
 import anuled.vocabulary.Geo;
 import anuled.vocabulary.LED;
 import anuled.vocabulary.QB;
@@ -34,10 +30,10 @@ public final class LandsatGraph extends GraphBase {
 	 * searched first on any query (we can speed that up later)
 	 */
 	private Model dataCubeMeta = ModelFactory.createDefaultModel();
-	private Resource qbStructure, qbDSDefinition;
-	private final String prefix = "http://www.example.org/ANU-LED-example#";
 	private HDF5Dataset reader;
-	private Resource timeAP;
+/*	private Resource qbStructure, qbDSDefinition;
+	private final String prefix = "http://www.example.org/ANU-LED-example#";
+	private Resource timeAP;*/
 
 	public LandsatGraph(String h5Filename) {
 		super();
@@ -47,60 +43,16 @@ public final class LandsatGraph extends GraphBase {
 
 	/** Initialise metadata associated with this dataset. */
 	private void initMeta() {
-		// TODO: Make these dynamic/customisable. Should probably just use a
-		// Turtle file for metadata
-		Resource instrumentAP = addAttributeProperty("instrument",
-				GCMDInstrument.Instrument);
-		Resource satelliteAP = addAttributeProperty("satellite",
-				GCMDPlatform.PLATFORM);
-		timeAP = addAttributeProperty("time", XSD.dateTimeStamp);
-		Resource bandAP = addAttributeProperty("band", XSD.integer);
-
-		Function<Resource, Property> asProp = (Resource res) -> dataCubeMeta
-				.createProperty(res.getURI());
-		qbStructure = dataCubeMeta
-				.createResource(prefix + "landsatDataStructure")
-				.addProperty(RDF.type, QB.DataStructureDefinition)
-				// Jena has a really weird Property/Resource split. They're both
-				// URIs, so why can't I mix and match them?
-				.addProperty(asProp.apply(instrumentAP), GCMDInstrument.ETM)
-				.addProperty(asProp.apply(satelliteAP), GCMDPlatform.LANDSAT)
-				.addProperty(asProp.apply(bandAP),
-						dataCubeMeta.createTypedLiteral(4));
-		qbDSDefinition = dataCubeMeta.createResource(prefix + "landsatData")
-				.addProperty(RDF.type, QB.DataSet)
-				.addProperty(QB.structure, qbStructure);
-
-		addCSDimension("positionComponent", LED.location);
-		addCSDimension("timeComponent", timeAP);
-		addCSDimension("dataComponent", LED.imageData);
-
-		addCSAttribute("instrumentComponent", instrumentAP);
-		addCSAttribute("satelliteComponent", satelliteAP);
-		addCSAttribute("bandComponent", bandAP);
+		// TODO: Make metadata customisable. Should be able to pass in a Turtle
+		// file from the command line or something like that.
+		InputStream stream = LandsatGraph.class.getResourceAsStream("/cube-meta.ttl");
+		dataCubeMeta.read(stream, null, "TTL");
+		try {
+			stream.close();
+		} catch (IOException e) {
+			throw new RuntimeException("Error closing metadata stream", e);
+		}
 	}
-
-	private Resource addAttributeProperty(String name, Resource range) {
-		return dataCubeMeta.createResource(prefix + name)
-				.addProperty(RDFS.range, range);
-	}
-
-	private Resource addCSDimension(String name, Resource dimension) {
-		Resource rv = dataCubeMeta.createResource(prefix + name)
-				.addProperty(QB.dimension, dimension)
-				.addProperty(RDF.type, QB.ComponentSpecification);
-		qbDSDefinition.addProperty(QB.component, rv);
-		return rv;
-	}
-
-	private Resource addCSAttribute(String name, Resource attribute) {
-		Resource rv = dataCubeMeta.createResource(prefix + name)
-				.addProperty(QB.attribute, attribute)
-				.addProperty(RDF.type, QB.ComponentSpecification);
-		qbDSDefinition.addProperty(QB.component, rv);
-		return rv;
-	}
-
 	/** Convert a pixel into a WKT polygon */
 	/*
 	 * private String pixelToPolyWKT(TileReader.Pixel p) { double top =
@@ -135,8 +87,8 @@ public final class LandsatGraph extends GraphBase {
 				// pxModel.createTypedLiteral(p.pixel[0]))
 				// TODO: This should be an xsd:dateTime (so pass
 				// .createTypedLiteral a Java Calendar object)
-				.addProperty(pxModel.createProperty(timeAP.getURI()),
-						pxModel.createLiteral(""))
+//				.addProperty(pxModel.createProperty(timeAP.getURI()),
+//						pxModel.createLiteral(""))
 				.addProperty(LED.resolution, pxModel.createTypedLiteral(0.0))
 				// .addProperty(LED.bounds,
 				// pxModel.createTypedLiteral(pixelToPolyWKT(p),
