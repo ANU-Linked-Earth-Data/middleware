d1d6b4b155a25fe03d50c6cd37d9103abc5bfa59
Sam Toyer <sam@qxcv.net>
2016-06-30 11:49:55 +1000

    Quick hack to fix coverage results

---
+++ dynamic-store/src/test/java/anuled/dynamicstore/TestVocabularies.java
@@ -0,0 +1,31 @@
+package anuled.dynamicstore;
+
+import org.junit.Test;
+
+import anuled.vocabulary.GCMDInstrument;
+import anuled.vocabulary.GCMDPlatform;
+import anuled.vocabulary.Geo;
+import anuled.vocabulary.LED;
+import anuled.vocabulary.QB;
+
+import static org.junit.Assert.*;
+
+/**
+ * I'm going to be brutally honest here: this class is mostly a hack to drive up
+ * code coverage.
+ */
+public class TestVocabularies {
+	@Test
+	public void TestVocab() {
+		assertTrue(GCMDInstrument.ETM.getURI().startsWith(
+				"http://geobrain.laits.gmu.edu/ontology/2004/11/gcmd-instrument.owl#"));
+		assertTrue(GCMDPlatform.LANDSAT_7.getURI().startsWith(
+				"http://geobrain.laits.gmu.edu/ontology/2004/11/gcmd-platform.owl#"));
+		assertTrue(Geo.lat.getURI()
+				.startsWith("https://www.w3.org/2003/01/geo/wgs84_pos#"));
+		assertTrue(LED.value.getURI()
+				.startsWith("http://www.anulinkedearth.org/"));
+		assertTrue(QB.ComponentProperty.getURI()
+				.startsWith("http://purl.org/linked-data/cube#"));
+	}
+}
